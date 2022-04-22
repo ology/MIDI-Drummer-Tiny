2af2463edd6b96cd55e5511d4c82e845803d7143
Gene Boggs <gene@ology.net>
2022-04-22 12:09:32 -0700

    Bump version

--- lib/MIDI/Drummer/Tiny.pm
+++ lib/MIDI/Drummer/Tiny.pm
@@ -2,7 +2,7 @@ package MIDI::Drummer::Tiny;
 
 # ABSTRACT: Glorified metronome
 
-our $VERSION = '0.2004';
+our $VERSION = '0.2005';
 
 use Algorithm::Combinatorics qw(variations_with_repetition);
 use MIDI::Util qw(dura_size);
