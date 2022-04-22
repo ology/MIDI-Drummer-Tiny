c511faa55e675f9bf9fbe4ce5ac2cc708abeff7a
Gene Boggs <gene@ology.net>
2022-04-21 23:57:38 -0700

    Initial commit

---
+++ eg/syncopation/lesson-12.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Algorithm::Combinatorics qw(variations_with_repetition);
+use lib map { "$ENV{HOME}/sandbox/$_/lib" } qw(MIDI-Drummer-Tiny MIDI-Util);
+use MIDI::Drummer::Tiny;
+use MIDI::Util qw(dura_size);
+
+my $bpm = shift || 100;
+
+my $d = MIDI::Drummer::Tiny->new(
+    bpm    => $bpm,
+    file   => "$0.mid",
+    kick   => 'n36',
+    snare  => 'n40',
+    reverb => 15,
+);
+
+my $counter = 0;
+
+$d->sync(
+    \&snare,
+    \&kick,
+    \&hhat,
+);
+
+$d->write;
+
+sub snare {
+    combinatorial( $d->snare, {
+        vary => {
+            0 => sub {
+                $d->note( $d->quarter, $d->snare );
+            },
+            1 => sub {
+                $d->rest( $d->eighth );
+                $d->note( $d->eighth, $d->snare );
+            },
+            2 => sub {
+                $d->note( $d->eighth, $d->snare );
+                $d->note( $d->eighth, $d->snare );
+            },
+        },
+    });
+}
+
+sub kick {
+    steady( $d->kick );
+}
+
+sub hhat {
+    steady( $d->closed_hh );
+}
+
+sub steady {
+    my ( $instrument, $opts ) = @_;
+
+    $opts->{duration} ||= $d->quarter;
+
+    for my $n ( 1 .. $counter ) {
+        $d->note( $opts->{duration}, $instrument );
+    }
+}
+
+sub combinatorial {
+    my ( $instrument, $opts ) = @_;
+
+    $opts->{negate}   ||= 0;
+    $opts->{beats}    ||= 4;
+    $opts->{repeat}   ||= 4;
+    $opts->{duration} ||= $d->quarter;
+    $opts->{vary}     ||= {
+        0 => sub { $d->rest( $opts->{duration} ) },
+        1 => sub { $d->note( $opts->{duration}, $instrument ) },
+    };
+
+    my @items = $opts->{patterns}
+        ? $opts->{patterns}->@*
+        : sort map { join '', @$_ }
+            variations_with_repetition( [ keys $opts->{vary}->%* ], $opts->{beats} );
+
+    for my $pattern (@items) {
+        $pattern =~ tr/01/10/ if $opts->{negate};
+
+        for ( 1 .. $opts->{repeat} ) {
+            for my $bit ( split //, $pattern ) {
+                $opts->{vary}{$bit}->();
+                $counter += dura_size( $opts->{duration} );
+            }
+        }
+    }
+}
