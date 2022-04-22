4a5a69ef6681d9017852c2ad39942c2aea1f43d1
Gene Boggs <gene@ology.net>
2022-04-21 22:47:58 -0700

    Use combinatorics

--- eg/syncopation/lesson-03.pl
+++ eg/syncopation/lesson-03.pl
@@ -2,9 +2,10 @@
 use strict;
 use warnings;
 
-use List::Util qw(max);
-use lib map { "$ENV{HOME}/sandbox/$_/lib" } qw(MIDI-Drummer-Tiny);
+use Algorithm::Combinatorics qw(variations_with_repetition);
+use lib map { "$ENV{HOME}/sandbox/$_/lib" } qw(MIDI-Drummer-Tiny MIDI-Util);
 use MIDI::Drummer::Tiny;
+use MIDI::Util qw(dura_size);
 
 my $bpm = shift || 100;
 
@@ -16,6 +17,8 @@ my $d = MIDI::Drummer::Tiny->new(
     reverb => 15,
 );
 
+my $counter = 0;
+
 $d->sync(
     \&hhat,
     \&snare,
@@ -24,79 +27,54 @@ $d->sync(
 
 $d->write;
 
-sub hhat {
-    steady( $d->closed_hh );
-}
-
 sub snare {
-    bitwise( $d->snare );
+    combinatorial( $d->snare );
 }
 
 sub kick {
-    bitwise( $d->kick, {
+    combinatorial( $d->kick, {
         negate => 1,
     });
+}
+
+sub hhat {
+    steady( $d->closed_hh );
 }
 
 sub steady {
     my ( $instrument, $opts ) = @_;
 
-    $opts->{repeat}   ||= 4;
     $opts->{duration} ||= $d->quarter;
-    $opts->{digits}   ||= [ 0 .. 15 ];
 
-    my $limit;
-    if ( $opts->{patterns} ) {
-        my $width = length $opts->{patterns}->[0];
-        $limit = $width * $opts->{patterns}->@*;
-    }
-    else {
-        my $max = max $opts->{digits}->@*;
-        my $width = length sprintf '%.b', $max;
-        $limit = ++$max * $width * $opts->{repeat};
-    }
-
-    for my $n ( 1 .. $limit ) {
+    for my $n ( 1 .. $counter ) {
         $d->note( $opts->{duration}, $instrument );
     }
 }
 
-sub bitwise {
+sub combinatorial {
     my ( $instrument, $opts ) = @_;
 
     $opts->{negate}   ||= 0;
+    $opts->{beats}    ||= 4;
     $opts->{repeat}   ||= 4;
     $opts->{duration} ||= $d->quarter;
-    $opts->{digits}   ||= [ 0 .. 15 ];
-    $opts->{ifthen}   ||= sub {
-        $d->note( $opts->{duration}, $instrument );
-    };
-    $opts->{ifelse}   ||= sub {
-        $d->rest( $opts->{duration} );
+    $opts->{vary}     ||= {
+        0 => sub { $d->rest( $opts->{duration} ) },
+        1 => sub { $d->note( $opts->{duration}, $instrument ) },
     };
 
-    my ( $width, @items );
-    if ( $opts->{patterns} ) {
-        $width = length $opts->{patterns}->[0];
-        @items = $opts->{patterns}->@*;
-    }
-    else {
-        $width = length sprintf '%.b', max $opts->{digits}->@*;
-        @items = $opts->{digits}->@*;
-    }
+    my @items = $opts->{patterns}
+        ? $opts->{patterns}->@*
+        : sort map { join '', @$_ }
+            variations_with_repetition( [ keys $opts->{vary}->%* ], $opts->{beats} );
 
-    for my $i (@items) {
-        my $pattern = $opts->{patterns} ? $i : sprintf '%.*b', $width, $i;
+    for my $pattern (@items) {
         $pattern =~ tr/01/10/ if $opts->{negate};
 
         for ( 1 .. $opts->{repeat} ) {
             for my $bit ( split //, $pattern ) {
-                if ($bit) {
-                    $opts->{ifthen}->();
-                }
-                else {
-                    $opts->{ifelse}->();
-                }
+                $opts->{vary}{$bit}->();
+                $counter += dura_size( $opts->{duration} );
             }
         }
     }
