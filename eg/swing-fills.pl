0de8ae594b422894f11da6b69aecb29d85d3e2b5
Gene Boggs <gene@ology.net>
2025-02-04 02:24:12 -0800

    WIP

---
+++ eg/swing-fills.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use MIDI::Drummer::Tiny;
+use MIDI::Drummer::Tiny::SwingFills;
+
+my $d = MIDI::Drummer::Tiny->new(
+    soundfont => '/Users/gene/Music/FluidR3_GM.sf2',
+);
+my $f = MIDI::Drummer::Tiny::SwingFills->new;
+
+for my $i (1 .. $d->bars * 4) {
+    my $fill = $f->get_fill($d, $d->ride2);
+    if (($i % $d->bars == 0) && ($fill->{dura} == 4)) {
+        $fill->{fill}->();
+    }
+    else {
+        $d->note( $d->quarter, $d->ride2, $d->kick );
+        if (($i % $d->bars == 0) && ($fill->{dura} == 3)) {
+            $fill->{fill}->();
+        }
+        else {
+            $d->note( $d->triplet_eighth, $d->ride2 );
+            $d->rest( $d->triplet_eighth );
+            $d->note( $d->triplet_eighth, $d->ride2, $d->kick );
+            if (($i % $d->bars == 0) && ($fill->{dura} == 2)) {
+                $fill->{fill}->();
+            }
+            else {
+                $d->note( $d->quarter, $d->ride2, $d->snare );
+                if (($i % $d->bars == 0) && ($fill->{dura} == 1)) {
+                    $fill->{fill}->();
+                }
+                else {
+                    $d->note( $d->triplet_eighth, $d->ride2, $d->kick );
+                    $d->rest( $d->triplet_eighth );
+                    $d->note( $d->triplet_eighth, $d->ride2 );
+                }
+            }
+        }
+    }
+}
+
+# $d->write;
+$d->play_with_timidity;
+# $d->play_with_fluidsynth;
