57f511118768cc978156e655c821ad223a3802c5
Gene Boggs <gene@ology.net>
2026-01-09 15:49:50 -0800

    WIP

--- eg/beats.pl
+++ eg/beats.pl
@@ -7,9 +7,11 @@ use MIDI::Drummer::Tiny::Beats;
 
 my $d = MIDI::Drummer::Tiny->new;
 my $f = MIDI::Drummer::Tiny::Beats->new;
+my $beats = $f->all_beats($d);
 
-for my $n (1 .. )
-my $beat = $f->get_beat($d, $n);
-$beat->{beat}->() for 1 .. 4;
+for my $n (1 .. keys %$beats) {
+    my $beat = $f->get_beat($d, $n);
+    $beat->{beat}->() for 1 .. 4;
+}
 
 $d->play_with_timidity;
