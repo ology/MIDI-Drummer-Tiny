70e3349bbfec0ea3aad55c8dd4d4641c4660ef6b
Gene Boggs <gene@ology.net>
2026-01-09 12:02:28 -0800

    WIP

--- lib/MIDI/Drummer/Tiny/Beats.pm
+++ lib/MIDI/Drummer/Tiny/Beats.pm
@@ -32,11 +32,11 @@ Return a new C<MIDI::Drummer::Tiny::Beats> object.
 
 =head2 get_beat
 
- $beat = $f->get_beat($drummer_obj, $beat_number);
+  $beat = $f->get_beat($drummer, $beat_number);
 
 Return either the given beat subroutine or a random beat.
 
-A B<drummer_object> and a B<beat_number> are required.
+A B<drummer> object and a B<beat_number> are required.
 
 =cut
 
