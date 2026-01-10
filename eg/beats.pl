a1cc6c3fc0635f66cad73e869f0e836dfe05ec5e
Gene Boggs <gene@ology.net>
2026-01-09 21:22:56 -0800

    WIP

--- eg/beats.pl
+++ eg/beats.pl
@@ -2,17 +2,15 @@
 use strict;
 use warnings;
 
-use MIDI::Drummer::Tiny;
 use MIDI::Drummer::Tiny::Beats;
-
-my $d = MIDI::Drummer::Tiny->new;
 
 my $beats = MIDI::Drummer::Tiny::Beats->new;
 
-my $all = $beats->all_beats($d);
+my $all = $beats->all_beats;
 
 for my $n (1 .. keys %$all) {
-    my $beat = $beats->get_beat($d, $n);
+    my $beat = $beats->get_beat($n);
+    # print $beat->{name}, "\n";
     $beat->{beat}->() for 1 .. 4;
 }
 
