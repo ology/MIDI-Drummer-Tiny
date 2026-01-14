499c97127010ee9095a83ecdd81315e1e375cc8b
Gene Boggs <gene@ology.net>
2026-01-14 15:51:40 -0800

    WIP

--- eg/grooves.pl
+++ eg/grooves.pl
@@ -5,7 +5,8 @@ use warnings;
 use MIDI::Drummer::Tiny ();
 use MIDI::Drummer::Tiny::Grooves ();
 
-my $search = shift || 'rock';
+my $cat  = shift // 'rock';
+my $name = shift // '';
 
 my $d = MIDI::Drummer::Tiny->new(
     kick  => 36,
@@ -14,7 +15,7 @@ my $d = MIDI::Drummer::Tiny->new(
 my $grooves = MIDI::Drummer::Tiny::Grooves->new(drummer => $d);
 
 # my $all = $grooves->all_grooves;
-my $all = $grooves->search(cat => $search);
+my $all = $grooves->search(cat => $cat, name => $name);
 
 for my $n (keys %$all) {
     my $groove = $grooves->get_groove($n);
