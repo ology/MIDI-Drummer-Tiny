de42f9c0f063af515302739cb17e47ce56c7a3f9
Gene Boggs <gene.boggs@gmail.com>
2026-04-08 09:54:11 -0700

    WIP

--- eg/groove-patterns.pl
+++ eg/groove-patterns.pl
@@ -8,7 +8,7 @@ use MIDI::Drummer::Tiny::Grooves ();
 my $grooves = MIDI::Drummer::Tiny::Grooves->new(return_patterns => 1);
 
 my $set = $grooves->search({ cat => 'house' });
-print ddc $set;
+# print ddc $set;
 
 my %pattern = $set->{27}{groove}->();
 print ddc \%pattern;
